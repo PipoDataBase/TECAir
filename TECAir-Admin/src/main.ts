f8ed239f4420e651468bb02611450a69ff78fb62
Emanuel Marin <emanuel.marin@ncubo.com>
2023-10-10 22:23:22 -0600

    new syncfusion license

--- TECAir-Admin/src/main.ts
+++ TECAir-Admin/src/main.ts
@@ -2,7 +2,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { registerLicense } from '@syncfusion/ej2-base';
 
-registerLicense("Ngo9BigBOggjHTQxAR8/V1NHaF5cXmtCf1NpRGNGfV5yd0VFalxSTnZZUiweQnxTdEZiWX9ccHVUQ2JaV0NzXw==");
+registerLicense("Ngo9BigBOggjHTQxAR8/V1NHaF5cXmVCf1JpRGNGfV5yd0VFalhRTnVdUiweQnxTdEZiWX5fcHdRRmNfV01wWA==");
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
